021e4c5471d247fc8de3f7feea5ac965c1381e36
AnastasiiaRed <nika23041997@gmail.com>
2019-02-24 22:15:59 +0300

    password generation

--- user/settings.py
+++ user/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-SERVER_EMAIL = 'georg.16.best@gmail.com'
+SERVER_EMAIL = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = SERVER_EMAIL
-EMAIL_HOST_PASSWORD = 'qwertyytrewq'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
