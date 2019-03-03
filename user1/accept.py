b1f078c7c34c140e3ebe81a4c26c2d0fa76944da
AnastasiiaRed <nika23041997@gmail.com>
2019-03-03 17:54:37 +0300

    acceptance test

---
+++ user1/accept.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import django
+from django.middleware.csrf import CsrfViewMiddleware, get_token
+from django.test import Client
+
+django.setup()
+csrf_client = Client(enforce_csrf_checks=True)
+URL = 'http://127.0.0.1:8080/login'
+EMAIL= 'test-user@test.com'
+PASSWORD= 'XXXX'
+csrf_client.get(URL) 
+csrftoken = csrf_client.cookies['csrftoken']
+login_data = dict(email=EMAIL, password=PASSWORD, csrfmiddlewaretoken=csrftoken.value, next='/')
+r = csrf_client.post(URL, data=login_data, headers=dict(Referer=URL))
