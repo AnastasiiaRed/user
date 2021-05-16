b2068bdf246218981c80f6205ea97c11d9e648a8
Анастасия Редчиц <nika23041997@gmail.com>
2021-05-17 00:33:05 +0300

    test

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            " your PYTHONPATH environment variable? Did you "
+            " your PYTHONPATH environmet variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
